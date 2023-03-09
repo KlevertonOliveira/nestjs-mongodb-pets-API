d7389471aade4ad7212d13168896fe984077d6ed
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-03-09 16:00:04 -0300

    feat(pet): implement PetService's 'create' method

--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pet } from './schemas/pet.schema';
 import * as mongoose from 'mongoose';
+import { CreatePetDto } from './dtos/create-pet.dto';
 
 @Injectable()
 export class PetService {
   constructor(@InjectModel(Pet.name) private petModel: mongoose.Model<Pet>) {}
 
   async findAll(): Promise<Pet[]> {
-    const pets = await this.petModel.find();
-    return pets;
+    return await this.petModel.find();
+  }
+
+  async create(pet: CreatePetDto): Promise<Pet> {
+    return await this.petModel.create(pet);
   }
 }
