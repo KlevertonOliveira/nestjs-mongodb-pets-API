b5327fd617588947a3468f686c0a05bb813734c0
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-03-09 15:38:34 -0300

    chore(app): add PetModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PetModule } from './pet/pet.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    PetModule,
   ],
   controllers: [AppController],
   providers: [AppService],
